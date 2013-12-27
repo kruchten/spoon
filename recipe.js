5b27244a80932d21a8d075d14f4d3695f1a985c9
Nicolas Kruchten <nicolas@datacratic.com>
2013-12-26 22:05:03 -0800

    much more compact

--- recipe.js
+++ recipe.js
@@ -1,31 +1,19 @@
 $(function() {
     $.getJSON("recipe.json", function(recipe) {
         $("body").append(
-            $("<div>")
-                .append($("<h1>").text(recipe.title))
-
-                .append($("<h2>").text("Ingredients"))
-                .append((function(){
-
-                    var ingredients = $("<ul>");
-                    recipe.ingredients.forEach(function(i){
-                        ingredients.append($("<li>").text( i.quantity + " of " + i.thing ));
-                    });
-                    return ingredients;
-
-                })())
-
-                .append($("<h2>").text("Steps"))
-                .append((function(){
-
-                    var steps = $("<ol>");
-
-                    recipe.steps.forEach(function(i){
-                        steps.append($("<li>").text(i));
-                    });
-                    return steps;
-
-                })())
+            $("<h1>").text(recipe.title),
+            $("<h2>").text("Ingredients"),
+            $("<ul>").append(
+                $.map(recipe.ingredients, function(i){ 
+                    return $("<li>").text( i.quantity + " of " + i.thing ); 
+                })
+            ),
+            $("<h2>").text("Steps"),
+            $("<ul>").append(
+                $.map(recipe.steps, function(i){
+                    return $("<li>").text(i); 
+                })
+            )
         );
     });
 });
